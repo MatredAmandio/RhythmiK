a7a61c61f1498399a78185a20205526b60795bcb
MatredAmandio <Matredamandio@hotmail.com>
2026-09-03 15:35:30 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='rhythmik-v4';const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{let x=r.clone();caches.open(CACHE).then(z=>z.put(e.request,x));return r}).catch(()=>caches.match('./index.html'))))});
